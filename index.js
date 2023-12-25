b5b6ba5b83ee4820705f1bc162ff51351729e4b5
gggaravind <57027269+gggaravind@users.noreply.github.com>
2023-12-25 15:23:01 +0530

    Update index.js
    1st Try

--- index.js
+++ index.js
@@ -4,3 +4,111 @@ http.createServer(function (req, res) {
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/neuropsych-platform', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Passport setup
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: 'your-client-id',
+  clientSecret: 'your-client-secret',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+},
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// Serve static files
+app.use(express.static('public'));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const messages = [];
+
+// Socket.io events
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  // Send chat history to the newly connected user
+  socket.emit('chat history', messages);
+
+  let chatType = 'community'; // Default chat type
+
+  // Handle chat type selection
+  socket.on('set chat type', (type) => {
+    chatType = type;
+    if (type === 'private') {
+      socket.join(socket.id); // Join a room based on user's socket ID
+    } else {
+      socket.leave(socket.id); // Leave any private room
+    }
+  });
+
+  // Handle chat messages
+  socket.on('chat message', (msg) => {
+    messages.push(msg);
+    io.emit('chat message', msg);
+  });
+
+  // Handle setting user's name
+  socket.on('set name', (name) => {
+    socket.username = name;
+  });
+
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
